9d2bfa5282455ebcc4c9c2cdc87b38a5bced9dc2
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2024-12-04 22:22:03 -0500

    Create stack component

---
+++ src/components/shared/Stack/Stack.tsx
@@ -0,0 +1,24 @@
+import classNames from '../../../js/utils/classNames'
+import styles from './Stack.module.css'
+
+export interface StackProps extends React.ComponentProps<'div'> {
+  gutter?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl'
+}
+
+export default function Stack({ children, gutter = 'md' }: StackProps) {
+  return (
+    <div
+      className={classNames(styles.trackifyStack, {
+        [styles.trackifyStackXXS]: gutter === 'xxs',
+        [styles.trackifyStackXS]: gutter === 'xs',
+        [styles.trackifyStackSM]: gutter === 'sm',
+        [styles.trackifyStackMD]: gutter === 'md',
+        [styles.trackifyStackLG]: gutter === 'lg',
+        [styles.trackifyStackXL]: gutter === 'xl',
+        [styles.trackifyStackXXL]: gutter === 'xxl',
+      })}
+    >
+      {children}
+    </div>
+  )
+}
