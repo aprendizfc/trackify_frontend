c55530b184294e915b5706b95c68163503ad0640
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2024-12-04 22:21:35 -0500

    Create label component

---
+++ src/components/shared/Label/Label.tsx
@@ -0,0 +1,22 @@
+import cx from '../../../js/utils/classNames'
+import styles from './Label.module.css'
+
+export interface LabelProps extends React.ComponentProps<'label'> {
+  isVisible?: boolean
+}
+
+export default function Label({
+  children,
+  htmlFor,
+  isVisible = true,
+  className,
+}: LabelProps) {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={cx(styles.label, className, { 'sr-only': !isVisible })}
+    >
+      {children}
+    </label>
+  )
+}
