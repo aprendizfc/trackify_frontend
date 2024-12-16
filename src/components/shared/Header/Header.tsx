98bc9dafc45bc4931a98bbd7befbee6f76d51eb3
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2024-12-16 15:47:29 -0500

    Display avatar

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,16 +1,24 @@
 import { NavLink } from 'react-router'
 import styles from './Header.module.css'
 import Container from '../Container/Container'
+import Avatar from '../Avatar/Avatar'
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Container>
-        <nav className={styles.nav}>
-          <NavLink to="/">Dashboard</NavLink>
-          <NavLink to="/accounts">Accounts</NavLink>
-          <NavLink to="/records">Records</NavLink>
-        </nav>
+        <div className={styles.headerWrapper}>
+          <nav className={styles.nav}>
+            <NavLink to="/">Dashboard</NavLink>
+            <NavLink to="/accounts">Accounts</NavLink>
+            <NavLink to="/records">Records</NavLink>
+          </nav>
+
+          <Avatar
+            img="https://mighty.tools/mockmind-api/content/cartoon/32.jpg"
+            altText="Gravatar"
+          />
+        </div>
       </Container>
     </header>
   )
