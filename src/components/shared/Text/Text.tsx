c9e7c5f6c632d12400110cc6117a69c7f5991fb0
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2025-01-03 22:02:32 -0500

    Add more variations

--- src/components/shared/Text/Text.tsx
+++ src/components/shared/Text/Text.tsx
@@ -4,15 +4,22 @@ import * as styles from './Text.module.css'
 interface TextProps extends React.ComponentProps<'p'> {
   children: React.ReactNode
   size?: 'md' | 'sm' | 'xs'
+  className?: string
+  weight?: 'light' | 'normal' | 'semibold' | 'bold'
+  color?: 'primary' | 'secondary'
 }
 
-export function Text({ children, size = 'md' }: TextProps) {
+export function Text({ children, size = 'md', className, weight, color }: TextProps) {
   return (
     <p
-      className={cx(styles.text, {
+      className={cx(styles.text, className, {
         [styles.textXS]: size === 'xs',
         [styles.textSM]: size === 'sm',
         [styles.textMD]: size === 'md',
+        [styles.textBold]: weight === 'bold',
+        [styles.textLight]: weight === 'light',
+        [styles.textSemibold]: weight === 'semibold',
+        [styles.textSecondary]: color === 'secondary',
       })}
     >
       {children}
