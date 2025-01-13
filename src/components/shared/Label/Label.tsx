36f9cc10db38ee10665d2d536cad6bd685c764d5
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2025-01-12 22:02:52 -0500

    Add isRequired option

--- src/components/shared/Label/Label.tsx
+++ src/components/shared/Label/Label.tsx
@@ -1,14 +1,17 @@
 import cx from '../../../js/utils/classNames/classNames'
+import { VisuallyHidden } from '../VisuallyHidden/VisuallyHidden'
 import * as styles from './Label.module.css'
 
 export interface LabelProps extends React.ComponentProps<'label'> {
   isVisible?: boolean
+  isRequired?: boolean
 }
 
 export default function Label({
   children,
   htmlFor,
   isVisible = true,
+  isRequired,
   className,
 }: LabelProps) {
   return (
@@ -17,6 +20,13 @@ export default function Label({
       className={cx(styles.label, className, { 'sr-only': !isVisible })}
     >
       {children}
+
+      {isRequired && (
+        <abbr className={styles.required}>
+          <span aria-hidden="true">*</span>
+          <VisuallyHidden>Required</VisuallyHidden>
+        </abbr>
+      )}
     </label>
   )
 }
