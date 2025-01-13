e2b6833763a7c8130d24e5cc58bef9ea3681db37
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2025-01-12 22:04:46 -0500

    Display error message

--- src/components/shared/TextField/TextField.tsx
+++ src/components/shared/TextField/TextField.tsx
@@ -1,36 +1,55 @@
 import cx from '../../../js/utils/classNames/classNames'
+import { ErrorInputMessage } from '../error-input-message/error-input-message'
 import Label from '../Label/Label'
 import * as styles from './TextField.module.css'
 
 export interface TextFieldProps extends React.ComponentProps<'input'> {
+  id: string
   label: string
   isLabelVisible?: boolean
   isInline?: boolean
+  isRequired?: boolean
+  value?: string
+  defaultValue?: string
+  errorMessage?: React.ReactNode
 }
 
 export default function TextField({
   label,
   id,
   isLabelVisible = true,
-  className,
   isInline,
+  isRequired,
+  className,
+  errorMessage,
   ...rest
 }: TextFieldProps) {
   return (
     <div
       className={cx({
-        [styles.trackifyTextFieldOuterContainer]: !isInline,
-        [styles.trackifyTextFieldInlineOuterContainer]: isInline,
+        [styles.textFieldOuterContainer]: !isInline,
+        [styles.textFieldInlineOuterContainer]: isInline,
       })}
     >
-      <div className={styles.trackifyTextFieldLabelWrapper}>
-        <Label htmlFor={id} isVisible={isLabelVisible}>
+      <div className={styles.textFieldLabelWrapper}>
+        <Label htmlFor={id} isVisible={isLabelVisible} isRequired={isRequired}>
           {label}
         </Label>
       </div>
 
-      <div className={styles.trackifyTextFieldWrapper}>
-        <input id={id} {...rest} className={cx(styles.trackifyTextField, className)} />
+      <div className={styles.textFieldWrapper}>
+        <input
+          {...rest}
+          aria-invalid={!!errorMessage}
+          aria-describedby={cx({
+            [`error-${id}`]: !!errorMessage,
+          })}
+          required={isRequired}
+          id={id}
+          className={cx(styles.textField, className)}
+        />
+
+        {errorMessage && <ErrorInputMessage message={errorMessage} id={id} />}
       </div>
     </div>
   )
