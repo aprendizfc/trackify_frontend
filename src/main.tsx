89bd48730a02dc463ba35fd289c32c3dc6799aaa
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2025-01-12 22:08:18 -0500

    Implement react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import AppRoutes from './routes.tsx'
 import './css/normalize.css'
 import './index.css'
 import './css/helpers.css'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 )
