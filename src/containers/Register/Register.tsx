90bf816fbbf031d81099e2160d113c3345a65aa4
Ricardo Ordoñez <rijordel@Ricardos-MacBook-Pro.local>
2025-01-12 22:08:43 -0500

    Implement sign up mutation

--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,30 +1,86 @@
 import { Link } from 'react-router'
-import Button from '../../components/shared/Button/Button'
-import Stack from '../../components/shared/Stack/Stack'
-import TextField from '../../components/shared/TextField/TextField'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Col } from '@/components/shared/grid/col/col'
+import { Row } from '@/components/shared/grid/row/row'
+import Button from '@/components/shared/Button/Button'
+import Stack from '@/components/shared/Stack/Stack'
+import TextField from '@/components/shared/TextField/TextField'
+import ExistingAccount from '@/components/shared/ExistingAccount/ExistingAccount'
+import { useSignUp } from './hooks/use-sign-up'
+import { registerSchema } from './schema/register-schema'
+import { RegisterData } from './register-types'
 import * as styles from './Register.module.css'
-import ExistingAccount from '../../components/shared/ExistingAccount/ExistingAccount'
 
 export default function Register() {
+  const { signUpMutation } = useSignUp()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterData>({ resolver: yupResolver(registerSchema) })
+
+  const onSubmit = (data: RegisterData) => {
+    signUpMutation.mutate(data)
+  }
+
   return (
     <>
       <h1>Register</h1>
 
       <div className={styles.container}>
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <Stack>
-            <TextField label="Email" id="email" name="email" />
+            <Row>
+              <Col>
+                <TextField
+                  {...register('firstName')}
+                  autoFocus
+                  isRequired
+                  label="First Name"
+                  id="firstName"
+                  errorMessage={errors.firstName?.message}
+                />
+              </Col>
 
-            <TextField label="Password" id="password" name="password" type="password" />
-
+              <Col>
+                <TextField
+                  {...register('lastName')}
+                  isRequired
+                  label="Last Name"
+                  id="lastName"
+                  errorMessage={errors.lastName?.message}
+                />
+              </Col>
+            </Row>
             <TextField
-              label="Confirm Password"
-              id="confirmPassword"
-              name="confirm-password"
-              type="password"
+              {...register('email')}
+              isRequired
+              type="email"
+              label="Email"
+              id="email"
+              errorMessage={errors.email?.message}
             />
 
-            <Button size="full" type="submit">
+            <TextField
+              {...register('password')}
+              isRequired
+              label="Password"
+              id="password"
+              type="password"
+              errorMessage={errors.password?.message}
+            />
+
+            <TextField
+              {...register('confirmPassword')}
+              isRequired
+              label="Confirm Password"
+              id="confirmPassword"
+              type="password"
+              errorMessage={errors.confirmPassword?.message}
+            />
+
+            <Button size="full" type="submit" isDisabled={signUpMutation.isPending}>
               Sign Up
             </Button>
           </Stack>
