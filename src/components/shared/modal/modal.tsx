94a30c4f132da067278dfe41aa0008efb533b444
Ricardo Ordoñez <richard11_71@outlook.com>
2025-02-10 21:52:19 -0500

    Use logic from use-modal

--- src/components/shared/modal/modal.tsx
+++ src/components/shared/modal/modal.tsx
@@ -3,29 +3,42 @@ import { X } from 'react-feather'
 import { FocusTrap } from '../focus-trap/focus-trap'
 import Button from '../Button/Button'
 import { Heading } from '../heading/heading'
+import { useModal } from './hooks/use-modal'
 
 interface ModalProps {
   children: React.ReactNode
   title: string
   hasCloseButton?: boolean
   onClose?: VoidFunction
+  closeOnOverlayClick?: boolean
+  shouldCloseOnEsc?: boolean
+  isOpen: boolean
 }
 
-export function Modal({ children, title, hasCloseButton = true, onClose }: ModalProps) {
-  const id = React.useId()
-  const modalTitleId = `modalTitle${id}`
+export function Modal({
+  children,
+  title,
+  hasCloseButton = true,
+  onClose,
+  closeOnOverlayClick = true,
+  shouldCloseOnEsc = true,
+  isOpen,
+}: ModalProps) {
+  const { getOverlayProps, getModalProps, getTitleProps } = useModal({
+    isOpen,
+    closeOnOverlayClick,
+    onClose,
+    shouldCloseOnEsc,
+    triggerElement: document.activeElement as HTMLElement,
+  })
+
+  if (!isOpen) return null
 
   return (
-    <div className="overlay">
+    <div {...getOverlayProps()} className="overlay">
       <FocusTrap>
-        <div
-          role="dialog"
-          aria-modal="true"
-          aria-labelledby={modalTitleId}
-          id={id}
-          className="modal"
-        >
-          <Heading appearance="h3" id={modalTitleId}>
+        <div {...getModalProps()} className="modal">
+          <Heading appearance="h3" {...getTitleProps()}>
             {title}
           </Heading>
 
@@ -37,7 +50,6 @@ export function Modal({ children, title, hasCloseButton = true, onClose }: Modal
               icon={<X role="presentation" focusable={false} />}
               variant="subtle"
               className="modal-close-btn"
-              // className={styles.modalCloseButton}
               onClick={onClose}
             />
           )}
