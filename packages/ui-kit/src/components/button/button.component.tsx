fa1befaec7673b6ee44379286f72d6e01b6b3d63
Ricardo Ordoñez <richard11_71@outlook.com>
2025-07-14 23:38:12 -0500

    Update types

--- packages/ui-kit/src/components/button/button.component.tsx
+++ packages/ui-kit/src/components/button/button.component.tsx
@@ -1,12 +1,14 @@
-export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
-  ...props
-}) => {
+interface ButtonProps extends React.ComponentProps<'button'> {
+  children: React.ReactNode
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
   return (
     <button
       className={`rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:ring-2 focus:ring-blue-400 focus:outline-none`}
       {...props}
     >
-      hola
+      {children}
     </button>
   )
 }
