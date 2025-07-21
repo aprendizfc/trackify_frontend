f16f02aa0bff69fd5e28249bddaf88d2bc482abd
Ricardo Ordoñez <richard11_71@outlook.com>
2025-07-20 22:21:03 -0500

    Adapt to use tanstack router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,24 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { routeTree } from './routeTree.gen'
 import './index.css'
-import App from './app.component'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+
+if (!rootElement.innerHTML) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
+}
