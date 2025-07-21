9099c923df4f46c41dd6fa55ab0267b7d6321135
Ricardo Ordoñez <richard11_71@outlook.com>
2025-07-20 22:10:09 -0500

    Ignore src/routes for file check

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,22 +1,30 @@
-import config from "@trackify/ui-eslint-config";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import config from '@trackify/ui-eslint-config'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default config.map((entry) => {
-  if (!entry.languageOptions?.parserOptions) {
-    return {
-      ...entry,
-      languageOptions: {
-        ...(entry.languageOptions ?? {}),
-        parserOptions: {
-          ...(entry.languageOptions?.parserOptions ?? {}),
-          project: "./tsconfig.app.json",
-          tsconfigRootDir: __dirname,
-        },
+  const updatedEntry = {
+    ...entry,
+    languageOptions: {
+      ...(entry.languageOptions ?? {}),
+      parserOptions: {
+        ...(entry.languageOptions?.parserOptions ?? {}),
+        project: './tsconfig.app.json',
+        tsconfigRootDir: __dirname,
       },
-    };
+    },
+    rules: {
+      ...(entry.rules ?? {}),
+      'check-filename/convention': [
+        'error',
+        {
+          ignoreDirs: ['src/routes'],
+        },
+      ],
+    },
   }
-  return entry;
-});
+
+  return updatedEntry
+})
