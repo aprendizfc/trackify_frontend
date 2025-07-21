e51be59e9d0278d1513e64a2306b55ed42572671
Ricardo Ordoñez <richard11_71@outlook.com>
2025-07-20 22:23:47 -0500

    Delete dead code

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import React from 'react'
 
@@ -6,21 +6,6 @@ export const Route = createRootRoute({
   component: () => (
     <React.Fragment>
       <Outlet />
-      {/* <div className="flex h-screen">
-        <div className="w-54 flex-col bg-gray-100 p-4">
-          <nav className="inline-flex flex-col gap-2">
-            <Link to="/">Home</Link>
-            <Link to="/accounts">Accounts</Link>
-            <Link to="/transactions">Transactions</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </nav>
-        </div>
-
-        <div className="flex-1 p-4">
-          <Outlet />
-        </div>
-      </div> */}
       <TanStackRouterDevtools />
     </React.Fragment>
   ),
