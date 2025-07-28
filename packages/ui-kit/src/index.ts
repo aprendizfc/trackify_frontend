c85d6a2004a80569b90e9bc8cf0184f8dd95f895
Ricardo Ordoñez <richard11_71@outlook.com>
2025-07-28 00:36:03 -0500

    Export card components

--- packages/ui-kit/src/index.ts
+++ packages/ui-kit/src/index.ts
@@ -1,3 +1,9 @@
+import './main.css'
+
 export * from './components/button/button.component'
 export * from './components/container/container.component'
 export * from './components/stack/stack.component'
+export * from './components/card/card.component'
+export * from './components/card/card-header.component'
+export * from './components/card/card-content.component'
+export * from './components/card/card-footer/card-footer.component'
