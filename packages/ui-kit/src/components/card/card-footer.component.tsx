04ef08f924c6c9e488f827fda9d35b63ce0e13a1
Ricardo Ordoñez <richard11_71@outlook.com>
2025-07-30 21:18:27 -0500

    Move directory

---
+++ packages/ui-kit/src/components/card/card-footer.component.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@trackify/ui-helpers'
+
+import type { CardFooterProps } from './card.type'
+
+export const CardFooter: React.FC<CardFooterProps> = ({
+  children,
+  className,
+  ...rest
+}) => {
+  return (
+    <div
+      className={cn('flex items-center px-6 [.border-t]:pt-6', className)}
+      data-slot="card-footer"
+      {...rest}
+    >
+      {children}
+    </div>
+  )
+}
--- packages/ui-kit/src/components/card/card-footer/card-footer.component.tsx
+++
@@ -1,7 +0,0 @@
-import type { CardFooterProps } from '../card.type'
-
-import { Stack } from '../../stack/stack.component'
-
-export const CardFooter: React.FC<CardFooterProps> = ({ children, ...rest }) => {
-  return <Stack {...rest}>{children}</Stack>
-}
