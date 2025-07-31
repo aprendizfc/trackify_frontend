3e2c0b8bed726e0b08765733e4b40ae6e68c866b
Ricardo Ordoñez <richard11_71@outlook.com>
2025-07-30 21:31:48 -0500

    Replace Index component with HomePage

--- apps/web/src/routes/(app)/_app.index.tsx
+++ apps/web/src/routes/(app)/_app.index.tsx
@@ -1,14 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-
-const Index: React.FC = () => {
-  return (
-    <div>
-      <h1 className="mb-4 text-4xl font-bold">Welcome to Trackify</h1>
-      <p className="text-lg text-gray-600">Your personal finance tracker</p>
-    </div>
-  )
-}
+import { HomePage } from '@trackify/ui-home'
 
 export const Route = createFileRoute('/(app)/_app/')({
-  component: Index,
+  component: HomePage,
 })
