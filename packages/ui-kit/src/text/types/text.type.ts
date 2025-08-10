bc470347de792d26ef96188fd5e2a737b1e13b34
Ricardo Ordoñez <richard11_71@outlook.com>
2025-08-09 19:34:10 -0500

    Remove dead code

--- packages/ui-kit/src/text/types/text.type.ts
+++ packages/ui-kit/src/text/types/text.type.ts
@@ -11,19 +11,3 @@ type PolymorphicComponentProps<T extends React.ElementType> = BaseProps &
   }
 
 export type TextProps<T extends React.ElementType> = PolymorphicComponentProps<T>
-
-// type BaseProps = {
-//   align?: 'center' | 'justify' | 'left' | 'right'
-//   color?: 'default' | 'error' | 'primary' | 'secondary' | 'success' | 'warning'
-//   size?: 'lg' | 'md' | 'sm' | 'xl' | 'xs'
-//   weight?: 'bold' | 'light' | 'medium' | 'normal' | 'semibold'
-// }
-
-// // Polymorphic props: T is the "as" component (e.g. 'p', 'span', etc.)
-// type PolymorphicComponentProps<T extends React.ElementType> = BaseProps &
-//   Omit<React.ComponentPropsWithoutRef<T>, 'as' | keyof BaseProps> & {
-//     as?: T
-//   }
-
-// // Props for the component
-// export type TextProps<T extends React.ElementType> = PolymorphicComponentProps<T>
