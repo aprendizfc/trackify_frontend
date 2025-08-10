15827d0d892e25b87558972dfaaca3d6baf6b43b
Ricardo Ordoñez <richard11_71@outlook.com>
2025-08-09 19:24:32 -0500

    Initial commit

---
+++ packages/ui-kit/src/badge/components/badge.component.tsx
@@ -0,0 +1,26 @@
+import { Slot } from '@radix-ui/react-slot'
+import { cn } from '@trackify/ui-helpers'
+
+import type { BadgeProps } from '../types/badge.type'
+
+import { badgeVariants } from '../helpers/badge.helper'
+
+export const Badge: React.FC<BadgeProps> = ({
+  appearance,
+  asChild,
+  children,
+  className,
+  size,
+  ...rest
+}) => {
+  const Component = asChild ? Slot : 'span'
+  return (
+    <Component
+      className={cn(badgeVariants({ appearance, className, size }))}
+      data-slot="badge"
+      {...rest}
+    >
+      {children}
+    </Component>
+  )
+}
