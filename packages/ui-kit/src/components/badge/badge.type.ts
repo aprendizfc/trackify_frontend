eb1464ee2f024629c0dee51320f4376bbe3d4ceb
Ricardo Ordoñez <richard11_71@outlook.com>
2025-08-09 19:32:50 -0500

    Delete directory

--- packages/ui-kit/src/components/badge/badge.type.ts
+++
@@ -1,9 +0,0 @@
-import type { VariantProps } from 'class-variance-authority'
-
-import type { badgeVariants } from './badge.helper'
-
-export interface BadgeProps
-  extends React.ComponentProps<'span'>,
-    VariantProps<typeof badgeVariants> {
-  asChild?: boolean
-}
