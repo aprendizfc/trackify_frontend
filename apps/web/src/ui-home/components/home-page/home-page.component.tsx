f1f3b7ec8664f8371d91fd37a88c02ead7f66fe2
Ricardo Ordoñez <richard11_71@outlook.com>
2025-08-09 19:36:37 -0500

    Display transactions overview

--- apps/web/src/ui-home/components/home-page/home-page.component.tsx
+++ apps/web/src/ui-home/components/home-page/home-page.component.tsx
@@ -1,11 +1,14 @@
 import { Stack } from '@trackify/ui-kit'
 
 import { AccountsOverviewSection } from '../accounts-overview-section/accounts-overview-section.component'
+import { TransactionsOverview } from '../transactions-overview/transactions-overview.component'
 
 export const HomePage: React.FC = () => {
   return (
-    <Stack direction="column">
+    <Stack direction="column" gap={10}>
       <AccountsOverviewSection />
+
+      <TransactionsOverview />
     </Stack>
   )
 }
