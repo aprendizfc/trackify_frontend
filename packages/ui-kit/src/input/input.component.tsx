09b1233609ce46bcd1bc42c73883059e6c62c6a6
Ricardo Ordoñez <richard11_71@outlook.com>
2025-11-08 22:13:21 -0500

    make eslint happy

--- packages/ui-kit/src/input/input.component.tsx
+++ packages/ui-kit/src/input/input.component.tsx
@@ -13,11 +13,6 @@ export const Input: React.FC<InputProps> = ({
 }) => {
   return (
     <Stack className="group relative">
-      {/* {startAdornment && (
-        <span className="inline-flex items-center justify-center rounded-tl rounded-bl border-t border-b border-l border-gray-400 py-2 pl-3 group-has-focus-within:border-blue-500 [&>svg]:size-3">
-          {startAdornment}
-        </span>
-      )} */}
       {startAdornment && (
         <span className="absolute top-1/2 left-3 -translate-y-1/2 [&>svg]:size-3">
           {startAdornment}
@@ -26,7 +21,6 @@ export const Input: React.FC<InputProps> = ({
 
       <input
         className={cn(
-          //   'w-full border-t border-b border-gray-400 px-3 py-2 outline-0 focus:border-blue-500',
           'w-full rounded border border-gray-400 px-3 py-2 outline-0 focus:border-blue-500',
           {
             'pl-11': startAdornment,
@@ -37,11 +31,6 @@ export const Input: React.FC<InputProps> = ({
         {...rest}
       />
 
-      {/* {endAdornment && (
-        <span className="rounded-tr rounded-br border-t border-r border-b border-gray-400 py-2 pr-3 group-has-focus-within:border-blue-500 [&>svg]:size-3">
-          {endAdornment}
-        </span>
-      )} */}
       {endAdornment && (
         <span className="absolute top-1/2 right-3 -translate-y-1/2 [&>svg]:size-3">
           {endAdornment}
